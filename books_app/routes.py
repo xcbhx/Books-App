2762f7fb534aae5cc0ab2cab38195d5b2f7222cc
Ceina Ellison <ceina.ellison@gmail.com>
2025-04-20 22:42:34 -0400

    added more queries

--- books_app/routes.py
+++ books_app/routes.py
@@ -2,7 +2,7 @@
 import os
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
-from books_app.models import Book, Author, Genre
+from books_app.models import Book, Author, Genre, User
 
 # Import app and db from events_app package so that we can run app
 from books_app import app, db
@@ -15,11 +15,10 @@ main = Blueprint("main", __name__)
 
 @main.route('/')
 def homepage():
-    # TODO: Make a query for all instances of 'User' and send to the template
-    return render_template('home.html')
+    users = User.query.all()
+    return render_template('home.html', users=users)
 
 @main.route('/profile/<username>')
 def profile(username):
-    # TODO: Make a query for the user with the given username, and send to the
-    # template
-    return render_template('profile.html', username=username)
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('profile.html', user=user)
